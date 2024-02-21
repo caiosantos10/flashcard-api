41d496904ec1c01f59558179cf4af6067bda8247
Caio Santos <caiosant10@gmail.com>
2024-02-21 14:37:18 -0300

    Initial setup

---
+++ flashcard-api/src/main/java/com/santos/flashcardapi/FlashcardApiApplication.java
@@ -0,0 +1,13 @@
+package com.santos.flashcardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardApiApplication.class, args);
+	}
+
+}
---
+++ flashcard-api/src/test/java/com/santos/flashcardapi/FlashcardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santos.flashcardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
