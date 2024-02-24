42179b4c413ba02c0fc4db9602bfbe80f17474a8
Caio Santos <caiosant10@gmail.com>
2024-02-24 15:39:04 -0300

    Creating card instancies for initialize database

--- flashcard-api/src/main/java/com/santos/flashcardapi/FlashcardApiApplication.java
+++ flashcard-api/src/main/java/com/santos/flashcardapi/FlashcardApiApplication.java
@@ -1,13 +1,33 @@
 package com.santos.flashcardapi;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.santos.flashcardapi.domain.Card;
+import com.santos.flashcardapi.repositories.CardRepository;
+
 @SpringBootApplication
-public class FlashcardApiApplication {
+public class FlashcardApiApplication implements CommandLineRunner{
+	@Autowired
+	private CardRepository repo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlashcardApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Card card1 = new Card("Teacher", "Professor(a)");
+		Card card2 = new Card("People", "Pessoas");
+		Card card3 = new Card("Sink", "Pia");
+		Card card4 = new Card("Word", "Palavra");
+		
+		repo.saveAll(Arrays.asList(card1, card2, card3, card4));
+		
 	}
 
 }
