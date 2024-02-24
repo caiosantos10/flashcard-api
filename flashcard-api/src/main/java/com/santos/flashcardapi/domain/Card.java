d4db3d9c2be0270f8f90d166468ead635a5f8037
Caio Santos <caiosant10@gmail.com>
2024-02-24 12:47:15 -0300

    H2 database, automatic database criation

--- flashcard-api/src/main/java/com/santos/flashcardapi/domain/Card.java
+++ flashcard-api/src/main/java/com/santos/flashcardapi/domain/Card.java
@@ -3,9 +3,17 @@ package com.santos.flashcardapi.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Card implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String front;
 	private String verse;
